cea7dc268fe052557daf98ae915d03bfe47986c3
Harshil Shah <harshil.shah@velotio.com>
2021-11-25 03:32:33 +0530

    - Initial commit with results looking the same as original

---
+++ trees/src/tree/index2.js
@@ -0,0 +1,29 @@
+import "./index.css";
+
+export default function Tree() {
+  return (
+    <div className="tree">
+      <ul className="root-list">
+        <li>
+          <span>root</span>
+          <ul>
+            <li>ant</li>
+            <li>
+              <span>bear</span>
+              <ul>
+                <li>cat</li>
+                <li>
+                  <span>dog</span>
+                  <ul>
+                    <li>elephant</li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li>frog</li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
+}
