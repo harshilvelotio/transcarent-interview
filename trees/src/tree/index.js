6e5fa92dd8cb5720b8791f851d8ff22e391a489d
Harshil Shah <harshil.shah@velotio.com>
2021-11-25 03:40:08 +0530

    Bonus 1: Added gridlines to compare original and new results

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -1,29 +1,46 @@
 import "./index.css";
 
+const GridLines = ({ depth }) =>
+  [...Array(depth)].map((_empty, index) => (
+    <span className={`indentation-level-${index + 1}`} />
+  ));
+
 export default function Tree() {
   return (
-    <div className="tree">
-      <ul className="root-list">
-        <li>
-          <span>root</span>
-          <ul>
-            <li>ant</li>
-            <li>
-              <span>bear</span>
-              <ul>
-                <li>cat</li>
-                <li>
-                  <span>dog</span>
-                  <ul>
-                    <li>elephant</li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li>frog</li>
-          </ul>
-        </li>
-      </ul>
-    </div>
+    <>
+      <GridLines depth={4} />
+      <div className="tree">
+        <p>root</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;ant</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;bear</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cat</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog</p>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant
+        </p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;frog</p>
+        <ul className="root-list">
+          <li>
+            <span>root</span>
+            <ul>
+              <li>ant</li>
+              <li>
+                <span>bear</span>
+                <ul>
+                  <li>cat</li>
+                  <li>
+                    <span>dog</span>
+                    <ul>
+                      <li>elephant</li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li>frog</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
